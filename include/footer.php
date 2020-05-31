83d75b56be57d9a203fa83f3da47942b611aa943
EGavilan-Media <45221488+EGavilan-Media@users.noreply.github.com>
2020-05-31 00:59:10 -0400

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,22 @@
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; EGavilan Media 2020</div>
+                    <div>
+                        <a target="blank" href="http://www.egavilanmedia.com/">egavilanmedia.com</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery-3.4.1.min.js"></script>
+    <script src="vendor/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+</body>
+
+</html>
